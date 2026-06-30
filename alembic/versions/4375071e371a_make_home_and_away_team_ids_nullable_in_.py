50ac841d80312a8817de7312df5cfdc00d3c9d7d
wvanlief <wvanliefferinge@gmail.com>
2026-06-30 13:04:25 +0200

    migration: alter home and away team ids to be nullable in fixtures table

---
+++ alembic/versions/4375071e371a_make_home_and_away_team_ids_nullable_in_.py
@@ -0,0 +1,30 @@
+"""make home and away team ids nullable in fixtures
+
+Revision ID: 4375071e371a
+Revises: 7fa565bb1f2c
+Create Date: 2026-06-30 13:03:59.695227
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '4375071e371a'
+down_revision: Union[str, Sequence[str], None] = '7fa565bb1f2c'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    # Make home_team_id and away_team_id nullable on Postgres
+    op.alter_column('fixtures', 'home_team_id', existing_type=sa.Integer(), nullable=True)
+    op.alter_column('fixtures', 'away_team_id', existing_type=sa.Integer(), nullable=True)
+
+
+def downgrade() -> None:
+    # Make home_team_id and away_team_id not nullable (if reverting)
+    op.alter_column('fixtures', 'home_team_id', existing_type=sa.Integer(), nullable=False)
+    op.alter_column('fixtures', 'away_team_id', existing_type=sa.Integer(), nullable=False)
